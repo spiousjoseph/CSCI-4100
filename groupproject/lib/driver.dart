5f0b2a34757f744849241937f33d31f0646a57d8
spiousjoseph <ghosttheassassin@hotmail.com>
2019-12-02 11:02:02 -0500

    Updated Passenger and Driver class

--- groupproject/lib/driver.dart
+++ groupproject/lib/driver.dart
@@ -1,1 +1,29 @@
 import 'package:flutter/material.dart';
+
+class Driver{
+  Driver({this.name, this.car, this.seats, this.license, this.reference});
+  String name;
+  String car;
+  int seats;
+  String license;
+  String reference;
+  //DocumentReference reference;
+
+  Driver.fromMap(Map<String,dynamic> map, {this.reference}){
+    this.name = map['name'];
+    this.car = map['car'];
+    this.seats = map['seats'];
+    this.license = map['license'];
+  }
+
+  Map<String,dynamic> toMap(){
+    return{
+      'name': this.name,
+      'car': this.car,
+      'seats':this.seats,
+      'license':this.license
+    };
+  }
+
+
+}
--- groupproject/lib/passenger.dart
+++ groupproject/lib/passenger.dart
@@ -1,1 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Passenger{
+  Passenger({this.name, this.reference});
+  String name;
+  String reference;
+  //DocumentReference reference;
+
+  Passenger.fromMap(Map<String,dynamic> map, {this.reference}){
+    this.name = map['name'];
+  }
+
+  Map<String,dynamic> toMap(){
+    return{
+      'name': this.name,
+    };
+  }
+
+
+}
