306b06c951b68a94e63f23067604c6cab8852a0d
Kelldav <brandon.angod@ontariotechu.net>
2019-12-02 20:31:48 -0500

    Added i18n Integration

---
+++ groupproject/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'package:groupproject/driverinvites.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_i18n/flutter_i18n_delegate.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Project',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Project'),
+      localizationsDelegates: [
+        FlutterI18nDelegate(
+          useCountryCode: false,
+          fallbackFile: 'en',
+          path: 'assets/i18n',
+        ),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+    );
+  }
+}
+
+
