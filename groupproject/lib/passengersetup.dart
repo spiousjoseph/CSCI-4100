21da86eaad4451302f3e17f95d1174ebb40f3216
Kelldav <brandon.angod@ontariotechu.net>
2019-12-02 22:04:57 -0500

    Added i18n Support

--- groupproject/lib/passengersetup.dart
+++ groupproject/lib/passengersetup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart' as prefix0;
+
 //import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'passengermap.dart';
 import 'package:geolocator/geolocator.dart';
@@ -7,38 +8,55 @@ import 'package:flutter/rendering.dart';
 import 'package:location/location.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:latlong/latlong.dart';
-
-
+import 'package:flutter_i18n/flutter_i18n.dart';
 
 class PassengerSetup extends StatefulWidget {
   @override
   PassengerSetupState createState() => PassengerSetupState();
 }
 
-class PassengerSetupState extends State<PassengerSetup>{
+class PassengerSetupState extends State<PassengerSetup> {
   TextEditingController _controllerName;
   TextEditingController _controller;
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Passenger Setup'),
-      ),
+      appBar: AppBar(title: Text('Passenger Setup'),
+          actions: <Widget>[
+        FlatButton(
+          child: Text('EN'),
+          onPressed: () {
+            Locale newLocale = Locale('en');
+            setState(() {
+              FlutterI18n.refresh(context, newLocale);
+            });
+          },
+        ),
+        FlatButton(
+            child: Text('FR'),
+            onPressed: () {
+              Locale newLocale = Locale('fr');
+              setState(() {
+                FlutterI18n.refresh(context, newLocale);
+              });
+            })
+      ]),
       body: Center(
         child: ListView(
           children: <Widget>[
             TextField(
               decoration: InputDecoration(
-                labelText: 'Name: ',
-                hintText: 'Enter your Name',
+                labelText: (FlutterI18n.translate(context, 'pregister.Name')),
+                hintText: (FlutterI18n.translate(context, 'register.NameHint')),
               ),
             ),
             TextField(
               decoration: InputDecoration(
-                labelText: 'Destination: ',
-                hintText: 'Enter your Destination',
+                labelText:
+                    (FlutterI18n.translate(context, 'pregister.Destination')),
+                hintText: (FlutterI18n.translate(
+                    context, 'pregister.DestinationHint')),
               ),
             ),
           ],
@@ -46,9 +64,8 @@ class PassengerSetupState extends State<PassengerSetup>{
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-
-
-          Navigator.push(context,
+          Navigator.push(
+            context,
             MaterialPageRoute(builder: (context) => PassengerMap()),
           );
         },
@@ -57,8 +74,5 @@ class PassengerSetupState extends State<PassengerSetup>{
     );
   }
 
-  _getCurrentLocation() {
-
-  }
-
+  _getCurrentLocation() {}
 }
