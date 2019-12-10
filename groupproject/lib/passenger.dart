bc1610b518e9f0946572419afe45cfb7ff5f8dfd
spiousjoseph <ghosttheassassin@hotmail.com>
2019-12-10 10:08:30 -0500

    Getting User Location

--- groupproject/lib/passenger.dart
+++ groupproject/lib/passenger.dart
@@ -1,11 +1,14 @@
 //import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:latlong/latlong.dart';
 //import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Passenger{
-  Passenger({this.name, this.reference});
+  Passenger({this.name, this.reference, this.destination, this.location});
   String name;
   String reference;
+  String destination;
+  LatLng location;
   //DocumentReference reference;
 
   Passenger.fromMap(Map<String,dynamic> map, {this.reference}){
--- groupproject/lib/passengersetup.dart
+++ groupproject/lib/passengersetup.dart
@@ -9,6 +9,7 @@ import 'package:location/location.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:latlong/latlong.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'passenger.dart';
 
 class PassengerSetup extends StatefulWidget {
   @override
@@ -17,7 +18,74 @@ class PassengerSetup extends StatefulWidget {
 
 class PassengerSetupState extends State<PassengerSetup> {
   TextEditingController _controllerName;
-  TextEditingController _controller;
+  TextEditingController _controllerDestination;
+  //Passenger passenger;
+
+
+  var _geolocator = Geolocator();
+  var _positionMessage = '';
+  var centre;
+  //String address = '301 Front St W, Toronto, ON';
+  String address = '160 Lionhead Trail, Toronto, ON';
+
+  void _updateLocation(userLocation) {
+
+    // geolocator plug-in:
+    setState(() {
+      _positionMessage = userLocation.latitude.toString() + ', ' + userLocation.longitude.toString();
+      centre =  LatLng(userLocation.latitude,userLocation.longitude);
+
+    });
+    print('New location: ${userLocation.latitude}, ${userLocation.longitude}.');
+    print('Centre location: ${centre.latitude}, ${centre.longitude}.');
+
+    _geolocator.placemarkFromCoordinates(userLocation.latitude, userLocation.longitude).then((List<Placemark> places) {
+      print('Reverse geocoding results:');
+      for (Placemark place in places) {
+        print('\t${place.name}, ${place.subThoroughfare}, ${place.thoroughfare}, ${place.locality}, ${place.subAdministrativeArea}');
+        print(place.position.latitude);
+        print(place.position.longitude);
+      }
+    });
+
+
+    _geolocator.placemarkFromAddress(address).then((List<Placemark> places) {
+      print('Forward geocoding results:');
+      for (Placemark place in places) {
+        print('\t${place.name}, ${place.subThoroughfare}, ${place.thoroughfare}, ${place.locality}, ${place.subAdministrativeArea}');
+      }
+    });
+
+  }
+
+
+  void updateDestination(destination){
+    //String address = '301 Front St W, Toronto, ON';
+    _geolocator.placemarkFromAddress(address).then((List<Placemark> places) {
+      print('Forward geocoding results:');
+      for (Placemark place in places) {
+        print('\t${place.name}, ${place.subThoroughfare}, ${place.thoroughfare}, ${place.locality}, ${place.subAdministrativeArea}');
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // this is called when the location changes
+    // geolocator plug-in version:
+
+    super.initState();
+    _geolocator.checkGeolocationPermissionStatus().then((GeolocationStatus geolocationStatus) {
+      print('Geolocation status: $geolocationStatus.');
+    });
+    _geolocator.getPositionStream(LocationOptions(accuracy: prefix0.LocationAccuracy.best, timeInterval: 5000))
+        .listen((userLocation) {
+      _updateLocation(userLocation);
+    }
+    );
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -46,12 +114,14 @@ class PassengerSetupState extends State<PassengerSetup> {
         child: ListView(
           children: <Widget>[
             TextField(
+              controller: _controllerName,
               decoration: InputDecoration(
                 labelText: (FlutterI18n.translate(context, 'pregister.Name')),
                 hintText: (FlutterI18n.translate(context, 'register.NameHint')),
               ),
             ),
             TextField(
+              controller: _controllerDestination,
               decoration: InputDecoration(
                 labelText:
                     (FlutterI18n.translate(context, 'pregister.Destination')),
@@ -64,9 +134,15 @@ class PassengerSetupState extends State<PassengerSetup> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          Passenger passenger = Passenger(
+            name: _controllerName.toString(),
+            destination: _controllerDestination.toString(),
+            location: centre,
+          );
+
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => PassengerMap()),
+            MaterialPageRoute(builder: (context) => PassengerMap(currentPosition: passenger,)),
           );
         },
         child: Icon(Icons.check),
@@ -74,5 +150,5 @@ class PassengerSetupState extends State<PassengerSetup> {
     );
   }
 
-  _getCurrentLocation() {}
+
 }
