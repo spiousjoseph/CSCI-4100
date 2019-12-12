8cb1cf09c96fc69e2694137fb3a36a8dd5d67e5e
MatrixPanda <zehan11s@hotmail.com>
2019-12-12 05:54:37 -0500

    first merge that runs

---
+++ groupproject/lib/passenger.dart
@@ -0,0 +1,26 @@
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:latlong/latlong.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Passenger{
+  Passenger({this.name, this.reference, this.destination, this.location, this.locationName});
+  String name;
+  String reference;
+  String destination;
+  String locationName;
+  LatLng location;
+  //DocumentReference reference;
+
+  Passenger.fromMap(Map<String,dynamic> map, {this.reference}){
+    this.name = map['name'];
+  }
+
+  Map<String,dynamic> toMap(){
+    return{
+      'name': this.name,
+    };
+  }
+
+
+}
